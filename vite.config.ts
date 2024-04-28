1a28ad5104938a0c087b1d28650259b1bf7f8bfb
Peter Collingridge <peter.collingridge@nanoporetech.com>
2024-04-28 11:44:32 +0100

    Update basenames

--- vite.config.ts
+++ vite.config.ts
@@ -3,8 +3,10 @@ import { defineConfig } from "vite";
 import tsconfigPaths from "vite-tsconfig-paths";
 
 export default defineConfig({
+  base: '/remix-puzzle/',
   plugins: [
     remix({
+      basename: '/remix-puzzle/',
       ssr: false,
     }),
     tsconfigPaths(),
