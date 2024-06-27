7a47e97b2cc0ca27d7652fd530f17fda77e22c85
Peter Collingridge <peter.collingridge@nanoporetech.com>
2024-06-27 22:25:16 +0100

    Draw selected circle on top of others

--- app/routes/arrange-circles.jsx
+++ app/routes/arrange-circles.jsx
@@ -3,32 +3,59 @@ import BasicLayout from '../components/BasicLayout';
 
 import '../components/shapes.css';
 
-function DragCircle({ svgRef, setParentClass, x = 0, y = 0, r = 24 }) {
-  const [pos, setPos] = useState({ x, y });
-  const [selected, setSelected] = useState(false);
+function Circle({ id, selected, select, x = 0, y = 0, r = 24 }) {
+  const className = 'draggable-shape' + (selected ? ' selected' : '');
 
-  const select = () => {
-    setSelected(true);
-    setParentClass('dragging');
+  return (
+    <circle
+      className={className}
+      onMouseDown={() => select(id)}
+      cx={x}
+      cy={y}
+      r={r}
+    />
+  );
+}
+
+function Puzzle({ svgRef }) {
+  const [className, setClassName] = useState('hover');
+  const [selectedItem, setSelectedItem] = useState(-1);
+
+  const [items, setItems] = useState([
+    { id: 0, x: -36, y: 0, r: 24 },
+    { id: 1, x:  36, y: 0, r: 24 },
+  ]);
+
+  const select = (id) => {
+    setSelectedItem(id);
+    setClassName('dragging');
+    // Move selected item to end of array so it's drawn in front of the rest
+    const newItems = items.filter((item) => item.id !== id);
+    const selectedItem = items.find((item) => item.id === id);
+    newItems.push(selectedItem);
+    setItems(newItems);
   };
 
   const deselect = () => {
-    setSelected(false);
-    setParentClass('hover');
+    setSelectedItem(-1);
+    setClassName('hover');
   };
 
   useEffect(() => {
-    if (!selected) {
+    if (selectedItem === -1) {
       return;
     }
 
     const CTM = svgRef.current.getScreenCTM();
+    const n = items.length - 1;
 
     const handleMouseMove = (event) => {
-      setPos((prev) => ({
-        x: prev.x + event.movementX / CTM.a,
-        y: prev.y + event.movementY / CTM.d,
-      }));
+      setItems((oldItems) => {
+        // Not sure why we have to divide by 2
+        oldItems[n].x += event.movementX / CTM.a / 2;
+        oldItems[n].y += event.movementY / CTM.d / 2;
+        return [...oldItems];
+      });
     };
 
     document.addEventListener("mousemove", handleMouseMove);
@@ -39,33 +66,17 @@ function DragCircle({ svgRef, setParentClass, x = 0, y = 0, r = 24 }) {
       document.removeEventListener("mousemove", handleMouseMove);
       document.removeEventListener("mouseup", deselect);
     };
-  }, [selected]);
-
-  const className = 'draggable-shape' + (selected ? ' selected' : '');
-
-  return (
-    <circle
-      className={className}
-      onMouseDown={select}
-      cx={pos.x}
-      cy={pos.y}
-      r={r}
-    />
-  );
-}
-
-function Puzzle({ svgRef }) {
-  const [className, setClassName] = useState('hover');
-
-  const circles = [
-    { x: -36, y: 0, r: 24 },
-    { x:  36, y: 0, r: 24 },
-  ];
+  }, [selectedItem]);
 
   return (
     <g className={`puzzle ${className}`}>
-      {circles.map((circle, index) => (
-        <DragCircle key={index} svgRef={svgRef} setParentClass={setClassName} {...circle} />
+      {items.map((item) => (
+        <Circle
+          key={item.id}
+          select={select}
+          selected={selectedItem === item.id}
+          {...item}
+        />
       ))}
     </g>
   )
